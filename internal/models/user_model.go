55cfcfd65e01ca538e2f2b62f750b8d65cb3db6d
metgag <moreehigh1@gmail.com>
2025-12-22 16:32:25 +0700

    feat: add base model and user model

---
+++ internal/models/user_model.go
@@ -0,0 +1,15 @@
+package models
+
+type UserRole string
+
+const (
+	RoleAdmin UserRole = "admin"
+	RoleUser  UserRole = "user"
+)
+
+type User struct {
+	BaseModel
+	Username string   `gorm:"type:varchar(100);uniqueIndex;not null"`
+	Password string   `gorm:"type:varchar(255);not null"`
+	Role     UserRole `gorm:"type:varchar(20);not null;default:'user'"`
+}
