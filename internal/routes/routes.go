b612b95e1ca6a39ac5b32e63cd669ad76dcdda48
metgag <moreehigh1@gmail.com>
2025-12-22 19:13:44 +0700

    feat: add admin protected route

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -6,23 +6,36 @@ import (
 	"gorm.io/gorm"
 
 	"github.com/metgag/procurement-api-example/internal/handlers"
+	"github.com/metgag/procurement-api-example/internal/middleware"
 )
 
 func InitRoutes(app *fiber.App, db *gorm.DB) {
 	app.Use(logger.New())
 
 	authHandler := handlers.NewAuthHandler(db)
+	supplierHandler := handlers.NewSupplierHandler(db)
 
 	api := app.Group("/api")
-	{
-		api.Get("/ping", func(c *fiber.Ctx) error {
-			return c.SendString("pong")
-		})
 
-		auth := api.Group("/auth")
-		{
-			auth.Post("/register", authHandler.Register)
-			auth.Post("/login", authHandler.Login)
-		}
+	// public routes
+	api.Get("/ping", func(c *fiber.Ctx) error {
+		return c.SendString("pong")
+	})
+
+	// auth routes
+	auth := api.Group("/auth")
+	{
+		auth.Post("/register", authHandler.Register)
+		auth.Post("/login", authHandler.Login)
+	}
+
+	// protected admin routes
+	admin := api.Group(
+		"/admin",
+		middleware.JWTProtected(),
+		middleware.RequireRole("admin"),
+	)
+	{
+		admin.Post("/suppliers", supplierHandler.Create)
 	}
 }
