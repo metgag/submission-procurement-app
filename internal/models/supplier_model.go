e3ffb764b5b536b94a765b5a5400c37e5c291d60
metgag <moreehigh1@gmail.com>
2025-12-22 21:53:12 +0700

    fix: gorm.model order at supplier model

--- internal/models/supplier_model.go
+++ internal/models/supplier_model.go
@@ -3,8 +3,8 @@ package models
 import "gorm.io/gorm"
 
 type Supplier struct {
+	gorm.Model
 	Name    string `gorm:"type:varchar(70);not null"`
 	Email   string `gorm:"type:varchar(100);uniqueIndex;not null"`
 	Address string `gorm:"type:varchar(255);not null"`
-	gorm.Model
 }
