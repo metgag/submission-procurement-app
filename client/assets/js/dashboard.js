876539f5b25298a371a9ec1b9fb474d6b9482f64
metgag <moreehigh1@gmail.com>
2025-12-24 21:52:00 +0700

    feat: add pagination at dashboard client side

--- client/assets/js/dashboard.js
+++ client/assets/js/dashboard.js
@@ -1,8 +1,14 @@
-const fetchInventory = () => {
+let currentPage = 1;
+const pageSize = 10;
+
+const fetchInventory = (page = 1) => {
+  currentPage = page;
+
   api
-    .get("/supplier-items")
+    .get(`/supplier-items?page=${currentPage}&page_size=${pageSize}`)
     .then((res) => {
       renderInventory(res.data);
+      renderPagination(res.meta);
     })
     .catch(() => {
       showToast({
@@ -42,5 +48,45 @@ const renderInventory = (items) => {
             </td>
         </tr>
     `);
+  });
+};
+
+const renderPagination = (meta) => {
+  const pagination = $("#pagination");
+  pagination.empty();
+
+  if (!meta || meta.total_pages <= 1) return;
+
+  pagination.append(`
+    <li>
+      <a href="#" class="px-3 py-1 rounded hover:opacity-70 hover:bg-gray-300" ${meta.page <= 1 ? "disabled" : ""} data-page="${meta.page - 1}"><</a>
+    </li>
+  `);
+
+  for (let i = 1; i <= meta.total_pages; i++) {
+    pagination.append(`
+      <li>
+        <a href="#" class="px-3 py-1 rounded ${
+          i === meta.page
+            ? "bg-indigo-600 text-white border-indigo-600 cursor-not-allowed"
+            : "hover:opacity-70 hover:bg-gray-300"
+        }" data-page="${i}">${i}
+        </a>
+      </li>
+    `);
+  }
+
+  pagination.append(`
+    <li>
+      <a href="#" class="px-3 py-1 rounded hover:opacity-70 hover:bg-gray-300" ${meta.page >= meta.total_pages ? "disabled" : ""} data-page="${meta.page + 1}">></a>
+    </li>
+  `);
+
+  $("#pagination a").click(function (e) {
+    e.preventDefault();
+    const page = $(this).data("page");
+    if (page && page !== currentPage) {
+      fetchInventory(page);
+    }
   });
 };
