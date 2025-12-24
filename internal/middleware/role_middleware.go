07db6593d47843bb6e922340e5eeb79c436e6e03
metgag <moreehigh1@gmail.com>
2025-12-24 12:48:23 +0700

    refactor: change param role middleware using variadic to allow multiple roles

--- internal/middleware/role_middleware.go
+++ internal/middleware/role_middleware.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"slices"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
 
@@ -8,9 +10,14 @@ import (
 	"github.com/metgag/procurement-api-example/internal/utils"
 )
 
-func RequireRole(role string) fiber.Handler {
+func RequireRole(allowedRoles ...string) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		user := c.Locals("user").(*jwt.Token)
+		user, ok := c.Locals("user").(*jwt.Token)
+		if !ok {
+			return c.Status(fiber.StatusUnauthorized).JSON(dto.ErrorResponse{
+				Message: "Invalid or missing token",
+			})
+		}
 
 		claims, ok := user.Claims.(*utils.JWTClaims)
 		if !ok {
@@ -19,12 +26,12 @@ func RequireRole(role string) fiber.Handler {
 			})
 		}
 
-		if claims.Role != role {
-			return c.Status(fiber.StatusForbidden).JSON(dto.ErrorResponse{
-				Message: "Access role denied",
-			})
+		if slices.Contains(allowedRoles, claims.Role) {
+			return c.Next()
 		}
 
-		return c.Next()
+		return c.Status(fiber.StatusForbidden).JSON(dto.ErrorResponse{
+			Message: "Access role denied",
+		})
 	}
 }
